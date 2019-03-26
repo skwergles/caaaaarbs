7c92f9ff57b1d64e6005669580b0761255a99dfe
Caden M <maw.cade@yahoo.com>
2019-03-26 10:30:38 -0700

    Update myscript.js
    Trolled

--- myscript.js
+++ myscript.js
@@ -33,29 +33,29 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bTrump\b/g, "Maw");
-	v = v.replace(/\btrump\b/g, "maw");
-	v = v.replace(/\bDonald J Trump\b/g, "Cade J Maw");
-	v = v.replace(/\bDonald John Trump\b/g, "Cade Jack Maw");
-	v = v.replace(/\bDonald Trump\b/g, "Cade Maw");
-	v = v.replace(/\bdonald trump\b/g, "cade maw");
-	v = v.replace(/\bdonald john trump\b/g, "cade jack maw");
-	v = v.replace(/\bdonald j trump\b/g, "cade j maw");
-	v = v.replace(/\bdonaldjtrump\b/g, "cadejmaw");
-	v = v.replace(/\bDonaldJTrump\b/g, "CadeJMaw");
-	v = v.replace(/\bDonald J. Trump\b/g, "Cade J. Maw");
-	v = v.replace(/\bdonald j. trump\b/g, "cade j. maw");
-	v = v.replace(/\brealdonaldtrump\b/g, "realcademaw");
-	v = v.replace(/\bRealDonaldTrump\b/g, "RealCadeMaw");
-	v = v.replace(/\bDonald\b/g, "Cade");
-	v = v.replace(/\bdonald\b/g, "cade");
-	v = v.replace(/\b@realdonaldtrump\b/g, "@realcademaw");
-	v = v.replace(/\b@RealDonaldTrump\b/g, "@RealCadeMaw");
-	v = v.replace(/\b@realDonaldTrump\b/g, "@realCadeMaw");
-	v = v.replace(/\b(@realDonaldTrump)\b/g, "(@realCadeMaw)");
-	v = v.replace(/\b#Trump2016\b/g, "#Maw2016");
-	v = v.replace(/\b#trump2016\b/g, "#maw2016");
-	v = v.replace(/\bDonald John Trump\b/g, "Cade Jack Maw");
+	v = v.replace(/\bTrump\b/g, "Randall");
+	v = v.replace(/\btrump\b/g, "randall");
+	v = v.replace(/\bDonald J Trump\b/g, "Benjamin T Randall");
+	v = v.replace(/\bDonald John Trump\b/g, "Benjamin Thomas Randall");
+	v = v.replace(/\bDonald Trump\b/g, "Benjamin Randall");
+	v = v.replace(/\bdonald trump\b/g, "benjamin randall");
+	v = v.replace(/\bdonald john trump\b/g, "benjamin thomas randall");
+	v = v.replace(/\bdonald j trump\b/g, "benjamin t randall");
+	v = v.replace(/\bdonaldjtrump\b/g, "benjaminjrandall");
+	v = v.replace(/\bDonaldJTrump\b/g, "BenjaminTRandall");
+	v = v.replace(/\bDonald J. Trump\b/g, "Benjamin T. Randall");
+	v = v.replace(/\bdonald j. trump\b/g, "benajmin t. randall");
+	v = v.replace(/\brealdonaldtrump\b/g, "realbenrandall");
+	v = v.replace(/\bRealDonaldTrump\b/g, "RealBenRandall");
+	v = v.replace(/\bDonald\b/g, "Benjamin");
+	v = v.replace(/\bdonald\b/g, "Benjamin");
+	v = v.replace(/\b@realdonaldtrump\b/g, "@realbenrandall");
+	v = v.replace(/\b@RealDonaldTrump\b/g, "@RealBenRandall");
+	v = v.replace(/\b@realDonaldTrump\b/g, "@realBenRandall");
+	v = v.replace(/\b(@realDonaldTrump)\b/g, "(@realBenRandall)");
+	v = v.replace(/\b#Trump2016\b/g, "#Randall2016");
+	v = v.replace(/\b#trump2016\b/g, "#randall2016");
+	v = v.replace(/\bDonald John Trump\b/g, "Benjamin Thomas Randall");
 	
 	textNode.nodeValue = v;
 }
